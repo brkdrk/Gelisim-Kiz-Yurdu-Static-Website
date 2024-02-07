d9e18904fb3aac625358518e436154f124d87922
brkdrk <burakdurak779@gmail.com>
2024-02-08 00:56:34 +0300

    Gelisim Project Eklendi

---
+++ gelisim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd static-web/
+aws s3 cp index.html s3://gelisimkizyurdu.aydin09.org/index.html
+aws s3 cp gelisim0.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim1.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim2.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim3.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim4.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim5.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim6.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim7.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim8.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim9.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim10.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim11.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim12.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim13.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim14.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim15.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim16.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim17.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim18.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim19.jpg s3://gelisimkizyurdu.aydin09.org/
+aws s3 cp gelisim20.jpg s3://gelisimkizyurdu.aydin09.org/
